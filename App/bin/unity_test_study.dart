38125918d972591ced1801327074df12736dfe31
wilker123 <josewilkerlucena123@gmail.com>
2021-12-14 16:59:22 -0300

    testing flutter test package commands

--- App/bin/unity_test_study.dart
+++ App/bin/unity_test_study.dart
@@ -1,10 +1,6 @@
 import 'package:tdd_in_flutter/person.dart';
 
-void main(List<String> arguments) {
-  final person = Person(name: "José", age: 30, height: 1.77, weight: 64.4);
-
-  // ignore: avoid_print
-  print('IMC deve vir 20.56: ${person.imc == 20.56}');
-  // ignore: avoid_print
-  print(person.imc);
+void main(){
+  final person = Person(name: "José", age: 30, height:1.77, weight: 64.4 );
+  print(person.name);
 }
--- App/lib/person.dart
+++ App/lib/person.dart
@@ -1,25 +1,19 @@
 import 'dart:math' as math;
-
-class Person {
+class Person{
   final String name;
   final int age;
   final double height;
   final double weight;
 
-  Person({
-    required this.name,
-    required this.age,
-    required this.height,
-    required this.weight,
-  });
+  Person({required this.name, required this.age, required this.height, required this.weight});
 
-  double get imc {
+  double get imc{
     var result = weight / math.pow(height, 2);
     result = result * 100;
     return result.roundToDouble() / 100;
   }
 
-  bool get isOlder {
+  bool get isOlder{
     return age >= 18;
   }
 }
