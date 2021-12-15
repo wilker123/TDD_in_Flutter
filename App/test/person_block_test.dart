03e046bba66b633fe2827e060474f82d43eb2f55
wilker123 <josewilkerlucena123@gmail.com>
2021-12-15 17:37:53 -0300

    Bug test bloc

--- App/test/person_block_test.dart
+++ App/test/person_block_test.dart
@@ -13,12 +13,31 @@ void main(){
   final bloc = PersonBloc(repository);
   final person = Person(id: 1, name: "José", age: 20, height: 1.77, weight: 64.4);
 
-  test("Deve retornar uma lista de person", (){
-    when(repository).calls(#getPerson).thenAnswer((_) async => <Person>[person,person ]);
-    bloc.add(PersonEvent.fetch);
-    expect(bloc, emitsInOrder([
+  test("Deve retornar uma lista de person", () async {
+    when(repository).calls(#getPerson).thenAnswer((_) async => <Person>[person,person]);
+   /*  expect(bloc, emitsInOrder([
       isA<PersonLoadingState>(),
-      isA<PersonListState>()
+      isA<PersonListState>(),
+    ])); */
+    bloc.add(PersonEvent.fetch);
+
+    await expectLater(bloc, emitsInOrder([
+      isA<PersonLoadingState>(),
+      isA<PersonListState>(),
+    ]));
+  });
+
+  test("Deve disparar um error", () async {
+    when(repository).calls(#getPerson).thenThrow(Exception("deu erro"));
+   /*  expect(bloc, emitsInOrder([
+      isA<PersonLoadingState>(),
+      isA<PersonListState>(),
+    ])); */
+    bloc.add(PersonEvent.fetch);
+
+    await expectLater(bloc, emitsInOrder([
+      isA<PersonLoadingState>(),
+      isA<PersonErrorState>(),
     ]));
   });
 }
